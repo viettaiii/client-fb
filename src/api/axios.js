f3cb22f89c331d2585b06ad834bf3a84f3d1b530
Viet Tai <viettai@MBVietTai.local>
2023-04-07 16:27:28 +0700

    Done

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,27 +1,20 @@
 import axios from "axios";
-import Cookies from "js-cookie";
+
 const httpsRequest = axios.create({
-    
   baseURL: "https://viettaiback-end-fb.herokuapp.com",
-  withCredentials: true,
-  timeout: 5000,
-  // headers: {
-  //   "Content-Type": "application/json",
-  //   Authorization: `Bearer ${Cookies.get("accessToken")}`,
-  // },
 });
 
-// httpsRequest.interceptors.request.use(
-//   (config) => {
-//     const token = localStorage.getItem("token");
-//     console.log(token);
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`;
-//     }
-//     return config;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
+httpsRequest.interceptors.request.use(
+  (config) => {
+    const token = JSON.parse(localStorage.getItem("token"));
+    console.log(token);
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 export default httpsRequest;
